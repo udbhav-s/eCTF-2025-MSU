0b900b09102e106f6baf50a345d30dc8db2c2710
udbhav <udbhavs03@gmail.com>
2025-03-05 00:44:19 -0500

    add code to save full subscription packages to flash (currently faulting)

--- rustdev/src/main.rs
+++ rustdev/src/main.rs
@@ -14,8 +14,7 @@ pub use hal::flc::{FlashError, Flc};
 pub use hal::gcr::clocks::{Clock, SystemClock};
 pub use hal::pac;
 use md5::{Digest, Md5};
-use ed25519_dalek::{Signature, Verifier, SigningKey, pkcs8::DecodePrivateKey};
-use modules::channel_manager::{save_subscription, SubscriptionError};
+use modules::channel_manager::check_subscription_valid_and_store;
 use modules::flash_manager::FlashManager;
 use modules::hostcom_manager::{
     read_ack, read_body, read_header, write_ack, write_debug, write_error, write_list,
@@ -23,22 +22,22 @@ use modules::hostcom_manager::{
 };
 use panic_halt as _; // Import panic handler
 
-use embedded_io::Write;
+// use embedded_io::Write;
 
 #[entry]
 fn main() -> ! {
     // Take ownership of the MAX78000 peripherals.
     let p = pac::Peripherals::take().unwrap();
-    // let core = pac::CorePeripherals::take().expect("Failed to take core peripherals");
+    let core = pac::CorePeripherals::take().expect("Failed to take core peripherals");
 
     // Initialize system peripherals and clocks.
     let mut gcr = hal::gcr::Gcr::new(p.gcr, p.lpgcr);
-    let ipo = hal::gcr::clocks::Ipo::new(gcr.osc_guards.ipo).enable(&mut gcr.reg);
+    let ipo: hal::gcr::clocks::Oscillator<hal::gcr::clocks::InternalPrimaryOscillator, hal::gcr::clocks::Enabled> = hal::gcr::clocks::Ipo::new(gcr.osc_guards.ipo).enable(&mut gcr.reg);
     let clks = gcr.sys_clk.set_source(&mut gcr.reg, &ipo).freeze();
 
     // Initialize a delay timer using the ARM SYST (SysTick) peripheral.
-    // let rate = clks.sys_clk.frequency;
-    // let mut delay = cortex_m::delay::Delay::new(core.SYST, rate);
+    let rate = clks.sys_clk.frequency;
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, rate);
 
     // Initialize and split the GPIO0 peripheral into pins.
     let gpio0_pins = hal::gpio::Gpio0::new(p.gpio0, &mut gcr.reg).split();
@@ -57,7 +56,7 @@ fn main() -> ! {
     for &b in b"Flash controller initialized!\r\n" {
         console.write_byte(b);
     }
-    // delay.delay_ms(1000);
+    delay.delay_ms(1000);
 
     let mut flash_manager = FlashManager::new(flc);
 
@@ -86,50 +85,10 @@ fn main() -> ! {
                 let _ = write_list(&mut console, &mut flash_manager);
             }
             x if x == MsgType::Subscribe as u8 => {
-                let key_der = b"\x30\x2e\x02\x01\x00\x30\x05\x06\x03\x2b\x65\x70\x04\x22\x04\x20\xdf\x05\x18\x15\x4c\xcc\xae\x9a\xb4\xf4\x8b\x5c\xb4\xc0\xfb\x59\x87\xec\x5b\x94\x98\x3a\x9a\x6c\x12\xd4\x8b\xc5\xb1\x19\xcb\x5b";
-                let signing_key = SigningKey::from_pkcs8_der(key_der);
-
-                if signing_key.is_err() {
-                    write_debug(&mut console, "Failed to parse key DER!\n");
-                    let _ = write_error(&mut console);
-                    continue
-                }
-
-                let verifying_key = signing_key.unwrap().verifying_key();
-
                 let _ = write_ack(&mut console);
                 let body = read_body(&mut console, hdr.length);
 
-                let msg_len = hdr.length as usize - 64;
-                let message = &body.data[..msg_len];
-                let signature = &body.data[msg_len..hdr.length as usize];
-                
-                let sig = Signature::from_slice(signature)
-                    .expect("Failed to parse signature");
-                
-                let result = verifying_key.verify(message, &sig);
-                
-                if result.is_err() {
-                    write_debug(&mut console, "Signature verification failed\n");
-                    let _ = write_error(&mut console);
-                    continue;
-                } else {
-                    write_debug(&mut console, "Signature verification succeeded!\n");
-                }
-
-                let decoder_id = u32::from_le_bytes(message[0..4].try_into().unwrap());
-                let start_ts = u64::from_le_bytes(message[4..12].try_into().unwrap());
-                let end_ts = u64::from_le_bytes(message[12..20].try_into().unwrap());
-                let channel_id = u32::from_le_bytes(message[20..24].try_into().unwrap());
-                // Note: bytes 24..36 contain the encryption nonce
-
-                // Debug prints for header fields
-                write!(console, "Decoder ID: {}\r\n", decoder_id).unwrap();
-                write!(console, "Start timestamp: {}\r\n", start_ts).unwrap();
-                write!(console, "End timestamp: {}\r\n", end_ts).unwrap();
-                write!(console, "Channel ID: {}\r\n", channel_id).unwrap();
-
-                // let result = save_subscription(&mut flash_manager, body);
+                let result = check_subscription_valid_and_store(&hdr, body, &mut flash_manager);
 
                 // Prepare a subscribe response header.
                 let resp_hdr = MessageHeader {
@@ -138,15 +97,16 @@ fn main() -> ! {
                     length: 0,
                 };
 
-                // if let Err(SubscriptionError::InvalidChannelId) = result {
-                //     let _ = write_error(&mut console);
-                // } else {
+                if let Err(_) = result {
+                    write_debug(&mut console, "Failed to verify subscription packet!");
+                    let _ = write_error(&mut console);
+                } else {
                     // Write the response header byte-by-byte.
                     for &b in bytemuck::bytes_of(&resp_hdr) {
                         console.write_byte(b);
                     }
                     let _ = read_ack(&mut console);
-                // }
+                }
             }
             x if x == MsgType::Decode as u8 => {
                 let _ = write_ack(&mut console);
--- rustdev/src/modules/channel_manager.rs
+++ rustdev/src/modules/channel_manager.rs
@@ -1,9 +1,12 @@
 use crate::modules::flash_manager::{FlashError, FlashManager, MyFlashError};
-use crate::modules::hostcom_manager::{ChannelInfo, MessageBody};
+use crate::modules::hostcom_manager::{ChannelInfo, MessageBody, MessageHeader};
+use bytemuck::{Pod, Zeroable};
+use ed25519_dalek::{Signature, Verifier, SigningKey, pkcs8::DecodePrivateKey};
 
 #[derive(Debug)]
 pub enum SubscriptionError {
     InvalidChannelId,
+    NoPageFound,
     FlashError(FlashError),
 }
 
@@ -13,79 +16,148 @@ impl From<FlashError> for SubscriptionError {
     }
 }
 
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct ChannelPassword {
+    pub node_trunc: u64,    // Upper 64 bits of the node in the tree (node_num // 2)
+    pub node_ext: u8,       // This will be 1 (left) or 2 (right) (node_num % 2)
+    pub password: [u8; 16],
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct ChannelPasswords {
+    pub contents: [ChannelPassword; 128],
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+pub struct ChannelSubscription {
+    pub info: ChannelInfo,
+    pub passwords: ChannelPasswords,
+}
+
 // const PAGE_SIZE: u32 = 0x2000;
 // const NUM_PAGES: usize = 8;
 // const BASE_ADDRESS: u32 = 0x1006_0000;
 
-pub fn save_subscription(
-    flash_manager: &mut FlashManager,
-    subscription: MessageBody,
-) -> Result<(), SubscriptionError> {
-    // Extract subscription fields from the MessageBody
-    let channel_id = u32::from_le_bytes(
-        subscription.data[(subscription.length - 4) as usize..subscription.length as usize]
-            .try_into()
-            .unwrap(),
-    );
+// Todo: Add more error types to SubscriptionError and use it here
+pub fn check_subscription_valid_and_store(
+    hdr: &MessageHeader,
+    body: MessageBody,
+    flash_manager: &mut FlashManager
+) -> Result<(), ()>  {
+    let key_der = b"\x30\x2e\x02\x01\x00\x30\x05\x06\x03\x2b\x65\x70\x04\x22\x04\x20\xdf\x05\x18\x15\x4c\xcc\xae\x9a\xb4\xf4\x8b\x5c\xb4\xc0\xfb\x59\x87\xec\x5b\x94\x98\x3a\x9a\x6c\x12\xd4\x8b\xc5\xb1\x19\xcb\x5b";
+    let signing_key = SigningKey::from_pkcs8_der(key_der);
 
-    // Check for zero channel_id and return a specific error
-    if channel_id == 0 {
-        return Err(SubscriptionError::InvalidChannelId);
+    if signing_key.is_err() {
+        return Err(());
     }
 
-    let end_timestamp = u64::from_le_bytes(
-        subscription.data[(subscription.length - 12) as usize..(subscription.length - 4) as usize]
-            .try_into()
-            .unwrap(),
-    );
-    let start_timestamp = u64::from_le_bytes(
-        subscription.data[(subscription.length - 20) as usize..(subscription.length - 12) as usize]
-            .try_into()
-            .unwrap(),
-    );
+    let verifying_key = signing_key.unwrap().verifying_key();
 
-    let new_sub = ChannelInfo {
+    let msg_len = hdr.length as usize - 64;
+    let message = &body.data[..msg_len];
+    let signature = &body.data[msg_len..hdr.length as usize];
+    
+    let sig_result = Signature::from_slice(signature);
+
+    if let Err(_) = sig_result {
+        return Err(());
+    }
+
+    let sig = sig_result.unwrap();
+    
+    let result = verifying_key.verify(message, &sig);
+    
+    if result.is_err() {
+        // write_debug(&mut console, "Signature verification failed\n");
+        return Err(());
+    } else {
+        // write_debug(&mut console, "Signature verification succeeded!\n");
+    }
+
+    let decoder_id = u32::from_le_bytes(message[0..4].try_into().unwrap());
+    let start_timestamp = u64::from_le_bytes(message[4..12].try_into().unwrap());
+    let end_timestamp = u64::from_le_bytes(message[12..20].try_into().unwrap());
+    let channel_id = u32::from_le_bytes(message[20..24].try_into().unwrap());
+    // Parse the 12-byte nonce from bytes 24-36
+    let mut nonce = [0u8; 12];
+    nonce.copy_from_slice(&message[24..36]);
+
+    // TODO: Check decoder id is valid
+    if decoder_id != 0 {
+
+    }
+
+    // Check if channel is channel 0
+    if channel_id == 0 {
+        return Err(());
+    }
+
+    // TODO: Decrypt channel passwords using the decoder key and nonce
+
+    // Parse the passwords into ChannelPasswords
+    let passwords = bytemuck::from_bytes::<ChannelPasswords>(&message[36..36+(128*25)]);
+
+    let channel_info = ChannelInfo {
         channel_id,
         start_timestamp,
-        end_timestamp,
+        end_timestamp
     };
 
-    // Fix the return syntax - using ? to propagate errors automatically
-    Ok(for i in 0..8 {
-        let addr = 0x1006_0000 + (i as u32 * 0x2000);
+    let channel_subscription = ChannelSubscription {
+        info: channel_info,
+        passwords: *passwords,
+    };
+
+    // Store the subscription
+    return save_subscription(flash_manager, channel_subscription).map_err(|_| ());
+}
+
+pub fn save_subscription(
+    flash_manager: &mut FlashManager,
+    subscription: ChannelSubscription,
+) -> Result<(), SubscriptionError> {
+
+    let channel_id = subscription.info.channel_id;
+
+    let mut page_addr: Option<u32> = None;
+    
+    for i in 0..8 {
+        // TODO: move flash base to a constants file, as well as flash magic
+        let addr = 0x1006_2000 + (i as u32 * 0x2000);
         match flash_manager.read_magic(addr) {
-            Ok(magic) => {
-                if magic != 0xABCD {
-                    // If magic doesn't match, consider this page empty.
-                    flash_manager
-                        .wipe_data(addr)
-                        .map_err(SubscriptionError::from)?;
-                    flash_manager
-                        .write_data(addr, 0xABCD, &new_sub)
-                        .map_err(SubscriptionError::from)?;
-                    return Ok(());
-                } else {
-                    // Magic present, so read the stored ChannelInfo.
-                    if let Ok(stored_sub) = flash_manager.read_data::<ChannelInfo>(addr) {
-                        if stored_sub.channel_id == channel_id {
-                            // Found a matching subscription.
-                            flash_manager
-                                .wipe_data(addr)
-                                .map_err(SubscriptionError::from)?;
-                            flash_manager
-                                .write_data(addr, 0xABCD, &new_sub)
-                                .map_err(SubscriptionError::from)?;
-                            return Ok(());
-                        }
+            // Magic present, the page is occupied
+            Ok(0xABCD) => {
+                if let Ok(stored_sub) = flash_manager.read_data::<ChannelInfo>(addr) {
+                    if stored_sub.channel_id == channel_id {
+                        // Found a matching subscription, overwrite it
+                        page_addr = Some(addr);
+                        break;
                     }
                 }
+            },
+            // Magic is not present, assume page unoccupied
+            Ok(_) => {
+                page_addr = Some(addr);
+                break;
             }
             Err(_) => {}
         }
-    })
+    }
 
-    // If we reach here, no slot was available
-    // Err(())
+    if let Some(addr) = page_addr {
+        flash_manager
+            .wipe_data(addr)?;
+        flash_manager
+            .write_data(addr, 0xABCD, &subscription)?;
+
+        return Ok(());
+    } else {
+        // No empty page or matching channel was found, this should not happen
+        return Err(SubscriptionError::NoPageFound);
+    }
 }
 
 pub fn read_channel(
@@ -93,7 +165,7 @@ pub fn read_channel(
     address: u32,
 ) -> Result<ChannelInfo, MyFlashError> {
     match flash_manager.read_magic(address) {
-        Ok(_) => flash_manager.read_data::<ChannelInfo>(address),
+        Ok(_) => Ok(flash_manager.read_data::<ChannelSubscription>(address)?.info),
         Err(e) => Err(MyFlashError::FlashError(e)),
     }
 }
