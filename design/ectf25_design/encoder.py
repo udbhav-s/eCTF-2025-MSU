5fbba12f9c593227fb9d039cfe21c9adaefa6be6
AdnanSlef <aself.school@gmail.com>
2025-03-24 12:23:49 -0400

    comment encoder

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -1,13 +1,9 @@
 """
-Author: Ben Janis
+Author: MSU Spartans
 Date: 2025
 
-This source file is part of an example system for MITRE's 2025 Embedded System CTF
-(eCTF). This code is being provided only for educational purposes for the 2025 MITRE
-eCTF competition, and may not meet MITRE standards for quality. Use this code at your
-own risk!
-
-Copyright: Copyright (c) 2025 The MITRE Corporation
+Satellite TV Frame Encoder
+With Encryption and Signing
 """
 
 import argparse
